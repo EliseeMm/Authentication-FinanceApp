04f7a91171d001f3d46cac93a723f5d654a5936f
elmathe022 <elmathe022@student.wethinkcode.co.za>
2023-09-05 14:05:17 +0200

    Request class builds client json objects

---
+++ src/main/java/ClientSide/JSONRequestBuilders/RequestJsonCreation.java
@@ -0,0 +1,37 @@
+package ClientSide.JSONRequestBuilders;
+
+import org.json.JSONObject;
+
+import java.util.Scanner;
+
+public class RequestJsonCreation {
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static JSONObject createRequest(){
+        String command = scanner.nextLine().toLowerCase();
+        JSONObject request = new JSONObject();
+        String requestString = "";
+        JSONObject requestsArguments = new JSONObject();
+
+        switch (command) {
+            case "login" -> {
+                requestsArguments = new LoginRequest().getRequest();
+            }
+            case "payment" -> {
+                requestsArguments = new SendMoneyRequest().getRequest();
+            }
+            case "signup" -> {
+                requestsArguments = new SignUpRequest().getRequest();
+            }
+            case "savings deposit","savings withdrawal","cash withdrawal","cash deposit" -> {
+                requestsArguments = new SimpleRequests().getRequest();
+            }
+            case "mini statement"-> {
+                requestsArguments = new MiniStatementRequest().getRequest();
+            }
+        }
+        request.put("Request",command);
+        request.put("Arguments",requestsArguments.get("arguments"));
+        return request;
+    }
+}
